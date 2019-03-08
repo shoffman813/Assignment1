94ae307a3a1c923810a3d7d381b4e94692a6071d
kelliemadson <kamadson@crimson.ua.edu>
2019-03-08 10:59:56 -0500

    Added comments and broke main up into functions

--- assignment1.cpp
+++ assignment1.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 using namespace std;
 
-int isDocNumber(string s,int count){
+int isDocNumber(string s,int count){	//function detects if a document number is encountered --> start a new vector
 	int i;
 	for(i=0;i<s.size();i++){
 		if(!isdigit(s[i])){
@@ -20,7 +20,7 @@ int isDocNumber(string s,int count){
 	}
 }
 
-int seenPreviously(string word, vector<string> seenWords){
+int seenPreviously(string word, vector<string> seenWords){	//function detects if term was in one of the previous documents or if it is a new term
 	int i;
 	for(i=0;i<seenWords.size();i++){
 		if(seenWords.at(i).compare(word) == 0){
@@ -31,75 +31,85 @@ int seenPreviously(string word, vector<string> seenWords){
 }	
 	
 vector<vector<int> > readDocs(vector<string> &seenWords){
-	int count = 2;
+	
+	//declarations
+
+	int count = 2;		//keeps track of what number document we're on (disregards the first one because we handle it differently)
 	ifstream documents;
 	documents.open("dataset.txt");
-	vector<int> doc_freq;	//each doc will have a frequency vector
-	vector<vector<int> > doc_matrix;
-	string doc_word;
+	vector<int> doc_freq;	//each doc will have a frequency vector containing term frequencies
+	vector<vector<int> > doc_matrix;	//matrix of dimensions #documents by #terms seen in corpus
+	string doc_word;	//stores each word read in from file
 	int i;
 	vector<int> temp;
 	vector<int> temp2;
-	documents >> doc_word;	//just the first doc number
-	documents >> doc_word;
-	while(!documents.eof()){
-		if(isDocNumber(doc_word,count)){
-			temp = doc_freq;
+
+	//start reading
+	
+	documents >> doc_word;	//just the first doc number (1)
+	documents >> doc_word;	//go ahead and read the first word in the first document
+	while(!documents.eof()){	//keep reading until end of file
+		if(isDocNumber(doc_word,count)){	//check to see if we found a document number
+			temp = doc_freq;		//if so, add the current document's frequency vector to the matrix
 			doc_matrix.push_back(temp);
-			for(i=0;i<seenWords.size();i++){
+			for(i=0;i<seenWords.size();i++){	//get the frequency vector ready for the next document by setting it to all zeros
 				doc_freq.at(i) = 0;
 			}
-			count++;
+			count++;	//increment count because we're moving on to the next document
 		}
-		else{
-			int index = seenPreviously(doc_word,seenWords);
-			if(index >= 0){
-				doc_freq.at(index) += 1;
+		else{		//it is not a document number, it is a term within a document
+			int index = seenPreviously(doc_word,seenWords);	//check to see if we have already seen the word before
+			if(index >= 0){	//we have seen the word before and know it's index number in the frequency vector
+				doc_freq.at(index) += 1;	//add one to that term's frequency
 			}
-			else{
-				seenWords.push_back(doc_word);
-				if(count > 2){
-					for(i=0;i<count-2;i++){
-						doc_matrix.at(i).push_back(0);
+			else{		//we have not seen the word before
+				seenWords.push_back(doc_word);	//add it to the seenWords vector for the future
+					for(i=0;i<count-2;i++){		//since we've added a column to this frequency vector, 
+						doc_matrix.at(i).push_back(0);	//go back to all the previous rows and add a 0 to make the dimensions match
 					}
-				}
-				doc_freq.push_back(1);
+				doc_freq.push_back(1);	//because it is a new term, we know the frequency is only 1, add this to the end of the vector
 			}
 		}
-		documents >> doc_word;
+		documents >> doc_word;	//read the next term in
 	}
-	doc_matrix.push_back(doc_freq);
+	doc_matrix.push_back(doc_freq);	//add the final document's frequency vector to the matrix
 	documents.close();
 	return doc_matrix;
 }
 
-vector<vector<string> > readQueries(){
-	int count = 2;
+vector<vector<string> > readQueries(){	//function does not create a frequency vector for each query; rather, it stores each query in a vector
+
+	//declarations
+
+	int count = 2;	//keeps track of what number query we're on (disregards the first one since it is handled differently)
 	ifstream queries;
 	queries.open("queries.txt");
 	vector<vector<string> > query_vector;
 	vector<string> query;
 	string query_word;
-	queries >> query_word;
-	query.push_back(query_word);
+	
+	//start reading
+
+	queries >> query_word;		//reads query number in
+	query.push_back(query_word);	//adds this to vector (the number is needed later)
 	queries >> query_word;
 	vector<string> temp;
 	while(!queries.eof()){
-		if(isDocNumber(query_word,count)){
+		if(isDocNumber(query_word,count)){	//uses this function to tell if term is a query number or not
 			temp = query;
-			query_vector.push_back(temp);
-			query.clear();
+			query_vector.push_back(temp);	//if so, add this query to the vector of queries and start reading the next one
+			query.clear();			//clear the vector variable to start the new one
 			count++;
 		}
-		query.push_back(query_word);
-		queries >> query_word;
+		query.push_back(query_word);		//add the number or term to the vector
+		queries >> query_word;			//read the next number or term in
 	}
-	query_vector.push_back(query);
+	query_vector.push_back(query);		//add the final query's vector to the vector of all queries
 	queries.close();
 	return query_vector;
 }
 
-int getWordIndex(string word,vector<string> wordList){
+int getWordIndex(string word,vector<string> wordList){	//takes the vector containing ALL seen words in the corpus and returns the index of string passed to the function
 	int i;
 	for(i=0;i<wordList.size();i++){
 		if(word.compare(wordList.at(i)) == 0)
@@ -108,7 +118,8 @@ int getWordIndex(string word,vector<string> wordList){
 	return -1;
 }
 
-void insertRanking(vector<vector<double> > &ranking,vector<double> pair){
+void insertRanking(vector<vector<double> > &ranking,vector<double> pair){	//inserts the document in the correct location in the ranking vector 
+										//according to it's cosine similarity value
 	int i;
 	for(i=0;i<ranking.size();i++){
 		if(pair.at(0) > ranking.at(i).at(0)){
@@ -116,121 +127,153 @@ void insertRanking(vector<vector<double> > &ranking,vector<double> pair){
 			return;
 		}
 	}
-	ranking.push_back(pair);
+	ranking.push_back(pair);	//the document with the smallest cosine similarity value gets added to the back
+	return;
+}
+
+vector<int> computeQueryFrequencyVector(vector<string> query, vector<string> words){
+	vector<int> frequency_vector;
+	int i;
+	for(i=0;i<words.size();i++){	//initialize frequency vector to all zeros
+		frequency_vector.push_back(0);
+	}
+	for(i=1;i<query.size();i++){                   //build query frequency vector so you can use this to do the dot product later
+        	int index = seenPreviously(query.at(i),words);     //checks to see if the term has been seen in any previous documents or queries
+        	if(index >= 0){         //it has been seen before
+                	frequency_vector.at(index) += 1;      //increment the value at that index by 1
+                }
+                else{           //it has not been seen before
+                	frequency_vector.push_back(1);        //add a 1 to the end of the query frequency vector
+                        words.push_back(query.at(i));  //add that word to the end of the list of seen words
+        	}
+	}
+	return frequency_vector;
+}
+
+double computeBinaryCosineSimilarity(vector<int> document,vector<int> query){
+	int doc_size = 0;   //stores value for ||d|| in cosine similarity equation
+        int numerator = 0;	//stores numerator in cosine similarity equation
+	int query_size = 0;	//stores value for ||q|| in cosine similarity equation
+        int i;
+	for(i=0;i<document.size();i++){         //for each term frequency in the vector
+        	if(query.at(i) > 0 && document.at(i) > 0)  //if both the query's freq vector and document's freq vector are > 0 at the ith index
+                	numerator += 1;        //calculate dot product the binary way by just adding 1 to the numerator (1 * 1 = 1)
+        }
+        for(i=0;i<document.size();i++){ //calculate the ||d|| in cosine similarity equation
+        	if(document.at(i) > 0)
+                	doc_size += 1;  
+        }
+	for(i=0;i<query.size();i++){
+		if(query.at(i) > 0)
+			query_size += 1;
+	}
+        double denominator = sqrt(query_size) * sqrt(doc_size);       //calculate denominator of cosine similarity equation
+        double cosine = numerator/denominator;
+	return cosine;
+}
+
+double computeCountCosineSimilarity(vector<int> document,vector<int> query){	//same function as above but for count term frequency
+	int doc_size = 0;  
+        int numerator = 0;
+	int query_size = 0;
+        int i;
+	for(i=0;i<document.size();i++){         //for each term frequency in the vector
+        	numerator += query.at(i) * document.at(i);        //calculate the dot product the count way
+        }
+        for(i=0;i<document.size();i++){ //calculate the ||d|| in cosine similarity equation
+        	doc_size += (document.at(i) * document.at(i));  //the count way
+        }
+	for(i=0;i<query.size();i++){
+		query_size += (query.at(i) * query.at(i));
+	}
+        double denominator = sqrt(query_size) * sqrt(doc_size);       //calculate denominator of cosine similarity equation the count way
+        double cosine = numerator/denominator;
+	return cosine;
+}
+
+void printResults(int option, vector<vector<double> > ranking1, vector<vector<double> > ranking2){
+	cout << "Document ranking using binary term frequency: ";
+        int i;
+	if(option == 1){        //want to see all of the ranked documents
+        	for(i=0;i<ranking1.size();i++){
+                	cout << ranking1.at(i).at(1) << " ";
+                }
+                cout << endl << "Document ranking using count of term frequencies: ";
+                for(i=0;i<ranking2.size();i++){
+                	cout << ranking2.at(i).at(1) << " ";
+                }
+        }
+        if(option == 2){        //only want to see the top 10 documents
+        	for(i=0;i<10;i++){
+                	cout << ranking1.at(i).at(1) << " ";
+                }
+                cout << endl << "Document ranking using count of term frequencies: ";
+                for(i=0;i<10;i++){
+                	cout << ranking2.at(i).at(1) << " ";
+                }
+        }
 	return;
 }
 	
 int main(){
-	vector<vector<int> > matrix;
-	vector<vector<string> > query_vector;
-	vector<string> words;
-	matrix = readDocs(words);
-	int i,j;
-	query_vector = readQueries();
-	int numerator1 = 0;
-	int numerator2 = 0;
-	int query_number;
-	vector<double> pair(2);	//holds the similarity value and document number
-	vector<vector<double> > doc_rank1; 	//holds document relevance ranking based on binary frequency
-	vector<vector<double> > doc_rank2;	//holds document relevance ranking based on count frequency
-	int index;
-	double cosine1,cosine2;
-	double size_query_vector1, size_query_vector2, size_doc_vector1, size_doc_vector2;
-	double denominator1,denominator2;
+	
+	//declarations
+	
+	vector<vector<int> > matrix;	//matrix where each row is a document frequency vector and each column represents a word seen in the corpus
+	
+	//FOR EXAMPLE, if a corpus contains 4000 unique words, there will be 4000 columns
+	//if "the" is the first word seen, then column 0 represents the term frequency of "the" for all documents in the corpus
+	
+	vector<vector<string> > query_vector;	//stores the query that the user wants to search
+	vector<string> words;			//contains a list of all words seen in the corpus, used to find index of a word in the matrix
+	int i,j;	//iterators
+	int query_number;	//stores query number the user wants to search
+	vector<double> pair(2);	//holds the cosine similarity value and its associated document number
+	vector<vector<double> > doc_rank1; 	//holds document relevance ranking list based on binary frequency
+	vector<vector<double> > doc_rank2;	//holds document relevance ranking list based on count frequency
 	vector<int> query_freq;	//frequency vector for the user's query
-	for(i=0;i<words.size();i++)
-		query_freq.push_back(0); 	//initialize the frequency vector to zeros
+	
+	//processing
+	
+	matrix = readDocs(words);
+	query_vector = readQueries();
 	vector<string> temp_q;		//holds the vector containing the requested query
 	cout << "Enter 1 if you would like to see the entire list of ranked documents for each query, enter 2 if you would only like to see the top 10." << endl;
 	int option;
 	cin >> option;
-	if(option != 1 && option != 2){
+	if(option != 1 && option != 2){		//error checking
 		cout << "Invalid input" << endl;
 		return 0;
 	}
 	cout << "Enter the number of the query you'd like to search, a number between 1 and 225, or enter 0 when you want to quit" << endl;
 	cin >> query_number;
-	if(query_number < 0 || query_number > 225){
+	if(query_number < 0 || query_number > 225){	//error checking
 		cout << "Invalid input" << endl;
 		return 0;
 	}
 	while(query_number > 0){
-		size_query_vector1 = 0;
-		size_query_vector2 = 0;
 		temp_q = query_vector.at(query_number-1); 	//stores the correct query in temp_q
-		for(i=1;i<temp_q.size();i++){			//build query frequency vector so you can use this to do the dot product later
-			index = seenPreviously(temp_q.at(i),words);	
-			if(index >= 0){
-				query_freq.at(index) += 1;
-			}
-			else{
-				query_freq.push_back(1);
-				words.push_back(temp_q.at(i));
-			}
-		}
-		for(i=0;i<query_freq.size();i++){	//calculate the ||q|| in cosine similarity equation
-                	if(query_freq.at(i) > 0)	//the binary way
-                        	size_query_vector1 += 1;
-                        size_query_vector2 += (query_freq.at(i) * query_freq.at(i)); //the count way
-                }
+		query_freq = computeQueryFrequencyVector(temp_q,words);	
 		for(j=0;j<matrix.size();j++){			//for each document in the corpus, calculate two similarities
-			size_doc_vector1 = 0;
-			size_doc_vector2 = 0;
-			numerator1 = 0;
-			numerator2 = 0;
-			vector<int> document = matrix.at(j);
-			for(i=0;i<document.size();i++){
-				if(query_freq.at(i) > 0 && document.at(i) > 0)
-					numerator1 += 1;	//calculate dot product the binary way
-				numerator2 += query_freq.at(i) * document.at(i);	//calculate the dot product the count way
-			}
-			for(i=0;i<document.size();i++){	//calculate the ||d|| in cosine similarity equation
-				if(document.at(i) > 0)
-					size_doc_vector1 += 1;	//the binary way
-				size_doc_vector2 += (document.at(i) * document.at(i));	//the count way
-			}
-			denominator1 = sqrt(size_query_vector1) * sqrt(size_doc_vector1);	//calculate denominator of cosine similarity equation the binary way
-			denominator2 = sqrt(size_query_vector2) * sqrt(size_doc_vector2);	//calculate denominator of cosine similarity equation the count way
-			cosine1 = numerator1/denominator1;
-			cosine2 = numerator2/denominator2;
-			pair.at(0) = cosine1;	//create pair holding the similarity value first, the document number second
+			pair.at(0) = computeBinaryCosineSimilarity(matrix.at(j),query_freq);
 			pair.at(1) = j+1;
-			insertRanking(doc_rank1,pair);	//insert it into a vector containing the ranking the binary way
-			pair.at(0) = cosine2;
+			insertRanking(doc_rank1,pair);
+			pair.at(0) = computeCountCosineSimilarity(matrix.at(j),query_freq);
 			pair.at(1) = j+1;
-			insertRanking(doc_rank2,pair);	//insert it into a vector containing the ranking the count way
+			insertRanking(doc_rank2,pair);
 		}
-		cout << "Document ranking using binary term frequency: ";
-		if(option == 1){
-			for(i=0;i<doc_rank1.size();i++){
-				cout << doc_rank1.at(i).at(1) << " ";
-			}
-			cout << endl << "Document ranking using count of term frequencies: ";
-			for(i=0;i<doc_rank2.size();i++){
-				cout << doc_rank2.at(i).at(1) << " ";
-			}
-		}
-		if(option == 2){
-			for(i=0;i<10;i++){
-				cout << doc_rank1.at(i).at(1) << " ";
-			}
-			cout << endl << "Document ranking using count of term frequencies: ";
-			for(i=0;i<10;i++){
-				cout << doc_rank2.at(i).at(1) << " ";
-			}
-		}
+		
+		printResults(option, doc_rank1, doc_rank2);
+		
 		doc_rank1.clear();
 		doc_rank2.clear();
 		cout << endl << "Enter the number of the query you'd like to search, a number between 1 and 225, or enter 0 when you want to quit" << endl;
         	cin >> query_number;
-		if(query_number < 0 || query_number > 225){
+		if(query_number < 0 || query_number > 225){	//error checking
 			cout << "Invalid input" << endl;
 			return 0;
 		}
 		query_freq.clear();
-		for(i=0;i<words.size();i++){
-			query_freq.push_back(0);
-		}
 	}
 	return 0;
 }
